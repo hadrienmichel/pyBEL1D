c48bbce3c5913cfc44fd264632a979f4ce5aed58
hadrienmichel <hadrien.michel@uliege.be>
2020-07-01 14:54:17 +0200

    Changes to the main code (new graphs) and to examples)

--- exampleDC.py
+++ exampleDC.py
@@ -5,6 +5,8 @@ import time # For simple timing measurements
 import numpy as np # For the initialization of the parameters
 from matplotlib import pyplot # For graphics on post-processing
 from utilities import Tools # For further post-processing
+from os import listdir
+from os.path import isfile, join
 
 # Parameters for the tested model
 # modelTrue = np.asarray([5.0, 0.05, 0.25, 0.1, 0.2])
@@ -23,27 +25,36 @@ for j in range(nParam):
                     stdPrior[ident] = stdUniform(priorDC[i,j*2],priorDC[i,j*2+1])
                     meansPrior[ident] = meansUniform(priorDC[i,j*2],priorDC[i,j*2+1])
                     ident += 1
-Dataset = np.loadtxt("Data/DC/Mirandola_InterPACIFIC/Average_interp60.txt")
+Dataset = np.loadtxt("Data/DC/Mirandola_InterPACIFIC/Average/Average_interp60_cuttoff.txt")
 FreqMIR = Dataset[:,0]
 Dataset = np.divide(Dataset[:,1],1000)# Phase velocity in km/s for the forward model
-ErrorModel = [0.05, 100]
+ErrorModel = [0.075, 20]
 
 # Attention, the maximum number of periods is 60 for the forward model! Keep the number of points low!
 
 # Show the noise model for the dataset:
 pyplot.plot(FreqMIR, Dataset,'b')
-pyplot.xlabel("Frequency [Hz]")
-pyplot.ylabel("Phase velocity [km/s]")
 dataNoisyU = Dataset + np.divide(ErrorModel[0]*Dataset*1000 + np.divide(ErrorModel[1],FreqMIR),1000)
 dataNoisyU2 = Dataset + 2*np.divide(ErrorModel[0]*Dataset*1000 + np.divide(ErrorModel[1],FreqMIR),1000)
 dataNoisyL = Dataset - np.divide(ErrorModel[0]*Dataset*1000 + np.divide(ErrorModel[1],FreqMIR),1000)
 dataNoisyL2 = Dataset - 2*np.divide(ErrorModel[0]*Dataset*1000 + np.divide(ErrorModel[1],FreqMIR),1000)
+DataPath = "Data/DC/Mirandola_InterPACIFIC/"
+files = [f for f in listdir(DataPath) if isfile(join(DataPath, f))]
+for currFile in files:
+    DatasetOther = np.loadtxt(DataPath+currFile)
+    DatasetOther = np.divide(DatasetOther[:,1],1000) # Dataset for surf96 in km/s
+    DatasetOther[DatasetOther==0] = np.nan
+    pyplot.plot(FreqMIR, DatasetOther,'ko')
 pyplot.plot(FreqMIR, dataNoisyU,'r')
 pyplot.plot(FreqMIR, dataNoisyL,'r')
 pyplot.plot(FreqMIR, dataNoisyU2,'r--')
 pyplot.plot(FreqMIR, dataNoisyL2,'r--')
+pyplot.plot(FreqMIR, Dataset,'b')
+pyplot.xlabel("Frequency [Hz]")
+pyplot.ylabel("Phase velocity [km/s]")
+pyplot.xscale('log')
+pyplot.yscale('log')
 pyplot.show()
-
 
 # Function to test the most direct approach:
 def test(nbModPre=1000):
@@ -56,13 +67,13 @@ def test(nbModPre=1000):
     print('Running PREBEL . . .')
     Prebel.run()
     # You can observe the relationship using:
-    Prebel.KDE.ShowKDE()
+    # Prebel.KDE.ShowKDE()
 
     # Then, since we know the dataset, we can initialize the "post-bel" operations:
     Postbel = BEL1D.POSTBEL(Prebel)
     # Run the operations:
     print('Sampling posterior . . .')
-    Postbel.run(Dataset=Dataset, nbSamples=1000, NoiseModel=ErrorModel)
+    Postbel.run(Dataset=Dataset, nbSamples=nbModPre, NoiseModel=ErrorModel)
 
     # All the operations are done, now, you just need to analyze the results (or run the iteration process - see next example)
     # Show the models parameters uncorrelated:
@@ -71,6 +82,13 @@ def test(nbModPre=1000):
     Postbel.ShowPostCorr(OtherMethod=Prebel.MODELS)
     # Show the depth distributions of the parameters with the RMSE
     Postbel.ShowPostModels(RMSE=True)
+    Postbel.ShowDataset(RMSE=True)
+    Postbel.KDE.ShowKDE(Xvals=Postbel.CCA.transform(Postbel.PCA['Data'].transform(np.reshape(Dataset,(1,-1)))))
+    
+    Prebel.ShowPreModels()
+    Prebel.ShowPriorDataset()
+    Postbel.ShowPostModels(RMSE=True,Best=1)
+    Postbel.ShowDataset(RMSE=True,Best=1)
     # Get key statistics
     means, stds = Postbel.GetStats()
     return means, stds
@@ -83,6 +101,7 @@ def testIter(nbIter=5):
     timings = np.zeros((nbIter,))
     start = time.time()
     diverge = True
+    distancePrevious = 1e10
     for idxIter in range(nbIter):
         if idxIter == 0: # Initialization
             TestCase = BEL1D.MODELSET().DC(prior=priorDC, Frequency=FreqMIR)
@@ -92,6 +111,7 @@ def testIter(nbIter=5):
             print(idxIter+1)
             PostbelTest = BEL1D.POSTBEL(PrebelIter)
             PostbelTest.run(Dataset=Dataset,nbSamples=nbModPre,NoiseModel=ErrorModel)
+            # PostbelTest.KDE.ShowKDE(Xvals=PostbelTest.CCA.transform(PostbelTest.PCA['Data'].transform(np.reshape(Dataset,(1,-1)))))
             means[idxIter,:], stds[idxIter,:] = PostbelTest.GetStats()
             end = time.time()
             timings[idxIter] = end-start
@@ -107,21 +127,26 @@ def testIter(nbIter=5):
             means[idxIter,:], stds[idxIter,:] = PostbelTest.GetStats()
             end = time.time()
             timings[idxIter] = end-start
-        diverge, distance = Tools.ConvergeTest(SamplesA=ModLastIter,SamplesB=PostbelTest.SAMPLES, tol=1e-5)
+        diverge, distance = Tools.ConvergeTest(SamplesA=ModLastIter,SamplesB=PostbelTest.SAMPLES, tol=1e-4)
         print('Wasserstein distance: {}'.format(distance))
-        if not(diverge):
+        if not(diverge) or (abs((distancePrevious-distance)/distancePrevious)*100<2):
+            # Convergence acheived if:
+            # 1) Distance below threshold
+            # 2) Distance does not vary significantly (less than 2%)
             print('Model has converged at iter {}!'.format(idxIter+1))
             break
+        distancePrevious = distance
         start = time.time()
     PostbelTest.ShowPostCorr(OtherMethod=PrebelIter.MODELS)
     PostbelTest.ShowPostModels(RMSE=True)
+    PostbelTest.ShowDataset(RMSE=True,Prior=False)
     timings = timings[:idxIter+1]
     means = means[:idxIter+1,:]
     stds = stds[:idxIter+1,:]
     paramnames = PostbelTest.MODPARAM.paramNames["NamesS"] # For the legend of the futur graphs
     return timings, means, stds, paramnames
 
-IterTest = True
+IterTest = False
 
 if IterTest:
     nbIter = 25
@@ -142,7 +167,37 @@ if IterTest:
     pyplot.show()
 
 if not(IterTest):
-    test()
+    test(nbModPre=1000)
+
+
+# nbModPre = 10000
+# print('Initializing . . .')
+# TestCase = BEL1D.MODELSET().DC(prior=priorDC, Frequency=FreqMIR)
+# # Then, we build the "pre-bel" operations using the PREBEL function
+# Prebel = BEL1D.PREBEL(TestCase,nbModels=nbModPre)
+# # We then run the prebel operations:
+# print('Running PREBEL . . .')
+# Prebel.run()
+# # You can observe the relationship using:
+# # Prebel.KDE.ShowKDE()
+
+# # Then, since we know the dataset, we can initialize the "post-bel" operations:
+# Postbel = BEL1D.POSTBEL(Prebel)
+# # Run the operations:
+# for idxUsed in range(len(files)):
+#     print('Current file: {}'.format(files[idxUsed]))
+#     DatasetOther = np.loadtxt(DataPath+files[idxUsed])
+#     DatasetOther = np.divide(DatasetOther[:,1],1000)
+#     Dataset = DatasetOther # Testing if works with other dataset than the mean (not the same span)
+#     Prebel.KDE.ShowKDE(Xvals=Postbel.CCA.transform(Postbel.PCA['Data'].transform(np.reshape(Dataset,(1,-1)))))
+#     try:
+#         print('Sampling posterior . . .')
+#         start = time.time()
+#         Postbel.run(Dataset=Dataset, nbSamples=10000, NoiseModel=ErrorModel)
+#         end = time.time()
+#         print('Dataset {} succeded in {} seconds!'.format(idxUsed+1,end-start))
+#     except:
+#         print('Dataset {} failed!'.format(idxUsed+1))
 
 
 
